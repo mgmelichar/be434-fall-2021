c7b804709aa05a0a47a9414d788644cc73385810
mgmelichar <mgmelichar@email.arizona.edu>
2021-09-13 18:35:43 -0700

    Assignment 2: added sum.py

--- assignments/02_sum/sum.py
+++ assignments/02_sum/sum.py
@@ -16,35 +16,11 @@ def get_args():
         description='Rock the Casbah',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
+    parser.add_argument('int',
+                        help='Numbers to add',
                         metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        nargs='+', 
+                        type=int)
 
     return parser.parse_args()
 
@@ -54,17 +30,13 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    string_int=[]
+
+    for int in args.int:
+        string_int.append(str(int))
+
+    print(' + '.join(string_int) + ' = ' + str(sum(args.int)))
 
 
 # --------------------------------------------------
